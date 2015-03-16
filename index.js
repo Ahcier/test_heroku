3273b0b266ebc7bea4f08d1289ed07b87cb39f34
ahcier <tenba2tekimen@gmail.com>
2015-03-16 08:58:56 +0000

    commit test from cloud9 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!konnitiha!cloud9');
+  response.send('Hello World!cloud9ahdfoa');
 });
 
 app.listen(app.get('port'), function() {
